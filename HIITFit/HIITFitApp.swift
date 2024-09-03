dd1c0f0cfd89349bb60736aea500a9c7149142ab
c-a-r-l-x <dev.carlx@gmail.com>
2024-09-03 18:12:31 -0400

    Initial Commit

---
+++ HIITFit/HIITFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIITFitApp.swift
+//  HIITFit
+//
+//  Created by carla on 03-09-24.
+//
+
+import SwiftUI
+
+@main
+struct HIITFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
