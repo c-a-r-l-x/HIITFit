d06ceb2467b11a869c6cfd716420a0d2153614e1
c-a-r-l-x <dev.carlx@gmail.com>
2024-09-20 12:34:06 -0300

    implement saving history data by writing it to a property list file

--- HIITFit/Models/HistoryStore.swift
+++ HIITFit/Models/HistoryStore.swift
@@ -9,9 +9,13 @@ struct ExerciseDay: Identifiable {
 class HistoryStore: ObservableObject {
   @Published var exerciseDays: [ExerciseDay] = []
   @Published var loadingError = false
+  var dataURL: URL {
+    URL.documentsDirectory.appendingPathComponent("history.plist")
+  }
   
   enum FileError: Error {
     case loadFailure
+    case saveFailure
   }
   
   init() {
@@ -26,13 +30,30 @@ class HistoryStore: ObservableObject {
     print("Initializing HistoryStore")
   }
   
+  // 1) Convert history data to a serialized property list format.
+  // 2) Write to disk using dataURL.
+  // 3) Conversion and writing may throw errors; catch them by throwing an error.
+  func save() throws {
+    let plistData = exerciseDays.map { exerciseDay in
+      [exerciseDay.id.uuidString, exerciseDay.date, exerciseDay.exercises]
+    }
+    do {
+      let data = try PropertyListSerialization.data( // 1
+        fromPropertyList: plistData, format: .binary, options: .zero
+      )
+      try data.write(to: dataURL, options: .atomic) // 2
+    } catch {
+      throw FileError.saveFailure // 3
+    }
+  }
+  
   func load() throws {
     throw FileError.loadFailure
   }
   
-  /// The date of the first element of exerciseDays is the user’s most recent exercise day.
-  /// 1) If today is the same as this date, append the current exerciseName to the exercises array of this ExerciseDay.
-  /// 2) If today is a new day, create a new ExerciseDay object and insert it at the beginning of the exerciseDays array.
+  // The date of the first element of exerciseDays is the user’s most recent exercise day.
+  // 1) If today is the same as this date, append the current exerciseName to the exercises array of this ExerciseDay.
+  // 2) If today is a new day, create a new ExerciseDay object and insert it at the beginning of the exerciseDays array.
   func addDoneExercise(_ exerciseName: String) {
     let today = Date()
     if let firstDate = exerciseDays.first?.date, today.isSameDay(as: firstDate) { // 1
@@ -43,6 +64,11 @@ class HistoryStore: ObservableObject {
         ExerciseDay(date: today, exercises: [exerciseName]),
         at: 0
       )
+    }
+    do {
+      try save()
+    } catch {
+      fatalError(error.localizedDescription)
     }
     print("History: ", exerciseDays)
   }
