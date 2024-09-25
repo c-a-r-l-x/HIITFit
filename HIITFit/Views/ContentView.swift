b71928d6917f9645a1b76d41795c5458bd52f813
c-a-r-l-x <dev.carlx@gmail.com>
2024-09-25 01:02:00 -0300

    create GradientBackgroundView to refactor ContentView

--- HIITFit/Views/ContentView.swift
+++ HIITFit/Views/ContentView.swift
@@ -4,15 +4,18 @@ struct ContentView: View {
   @SceneStorage("selectedTab") private var selectedTab = 9
   
   var body: some View {
-    TabView(selection: $selectedTab) {
-      WelcomeView(selectedTab: $selectedTab)
-        .tag(9)
-      ForEach(Exercise.exercises.indices, id: \.self) { index in
-        ExerciseView(selectedTab: $selectedTab, index: index)
-          .tag(index)
+    ZStack {
+      GradientBackgroundView()
+      TabView(selection: $selectedTab) {
+        WelcomeView(selectedTab: $selectedTab)
+          .tag(9)
+        ForEach(Exercise.exercises.indices, id: \.self) { index in
+          ExerciseView(selectedTab: $selectedTab, index: index)
+            .tag(index)
+        }
       }
+      .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
     }
-    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
   }
 }
 
