49f1bfdcbf246c092755d63c43c3c7757702c2a9
c-a-r-l-x <dev.carlx@gmail.com>
2024-09-26 11:31:42 -0300

    refactor HeaderView and HistoryView

--- HIITFit/Views/HistoryView.swift
+++ HIITFit/Views/HistoryView.swift
@@ -4,27 +4,40 @@ struct HistoryView: View {
   @EnvironmentObject var history: HistoryStore
   @Binding var showHistory: Bool
   
-  var body: some View {
-    ZStack(alignment: .topTrailing) {
+  var headerView: some View {
+    HStack {
+      Spacer()
+      Text("History")
+        .font(.title)
+      Spacer()
       Button(action: {
         showHistory.toggle()
       }, label: {
         Image(systemName: "xmark.circle")
           .font(.title)
-          .padding()
       })
-      VStack {
-        Text("History")
-          .font(.title)
-          .padding()
-        Form {
-          ForEach(history.exerciseDays) { day in
-            Section(header: Text(day.date.formatted(as: "MMM d")).font(.headline)) {
-              ForEach(day.exercises, id: \.self) { exercise in
-                Text(exercise)
-              }
-            }
-          }
+    }
+  }
+  
+  func dayView(day: ExerciseDay) -> some View {
+    Section(header: Text(day.date.formatted(as: "MMM d")).font(.headline)) {
+      exerciseView(day: day)
+    }
+  }
+  
+  func exerciseView(day: ExerciseDay) -> some View {
+    ForEach(day.exercises, id: \.self) { exercise in
+      Text(exercise)
+    }
+  }
+  
+  var body: some View {
+    VStack {
+      headerView
+        .padding()
+      Form {
+        ForEach(history.exerciseDays) { day in
+          dayView(day: day)
         }
       }
     }
